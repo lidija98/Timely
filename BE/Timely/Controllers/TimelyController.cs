2354d7fd5f43b6969513e02dd97db8d13b997b30
Lidija Čutura <93079523+lidija98@users.noreply.github.com>
2022-10-23 19:06:17 +0200

    Add controller

---
+++ BE/Timely/Controllers/TimelyController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Timely.Data;
+
+namespace Timely.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class TimelyController : Controller
+    {
+        private readonly DataContext _dataContext;
+
+        public TimelyController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProjects()
+        {
+            var projects = await _dataContext.Projects.ToListAsync();
+
+            return Ok(projects);
+        }
+    }
+}
+
