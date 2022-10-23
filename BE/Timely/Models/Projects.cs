0778754deccf32f1bebb5555f97b09c564c30861
Lidija Čutura <93079523+lidija98@users.noreply.github.com>
2022-10-23 17:42:41 +0200

    Delete and andd new

---
+++ BE/Timely/Models/Projects.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Timely.Models
+{
+    public class Projects
+    {
+        public string ProjectName { get; set; } = string.Empty;
+
+        public DateTimeOffset Start { get; set; }
+
+        public DateTimeOffset Stop { get; set; }
+
+        public DateTimeOffset Duration { get; set; }
+
+    }
+}
+
