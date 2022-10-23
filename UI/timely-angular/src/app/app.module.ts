46296606236d4193a11f1f1d333b1cb612da6e23
Lidija Čutura <93079523+lidija98@users.noreply.github.com>
2022-10-23 21:04:28 +0200

    Initial commit

--- UI/timely-angular/src/app/app.module.ts
+++ UI/timely-angular/src/app/app.module.ts
@@ -1,30 +1,18 @@
-import {HttpClientModule} from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TimelyComponent } from './timely/timely.component';
-import { ShowTimelyComponent } from './timely/show-timely/show-timely.component';
-import { AddEditTimelyComponent } from './timely/add-edit-timely/add-edit-timely.component';
-import { TimelyService } from './timely.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TimelyComponent,
-    ShowTimelyComponent,
-    AddEditTimelyComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule
+    AppRoutingModule
   ],
-  providers: [TimelyService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
