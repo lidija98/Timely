949e934c5c13499933b9ce8fe18b0582939ad655
Lidija Čutura <93079523+lidija98@users.noreply.github.com>
2022-10-24 21:19:31 +0200

    Modifications

--- UI/timely-angular/src/app/app-routing.module.ts
+++ UI/timely-angular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectListComponent } from './components/projects/project-list/project-list.component';
+import { TimerComponent } from './components/timer/timer/timer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TimerComponent },
+  { path: 'timer', component: ProjectListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- UI/timely-angular/src/app/app.module.ts
+++ UI/timely-angular/src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectListComponent } from './components/projects/project-list/project-list.component';
+import { TimerComponent } from './components/timer/timer/timer.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CompletedEntriesComponent } from './components/projects/completed-entries/completed-entries.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectListComponent,
+    TimerComponent,
+    CompletedEntriesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
